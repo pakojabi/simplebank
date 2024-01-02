ee3cc55a0ba4bae07e305f203b0951e7c46aaa95
Javier R <fjrpascual@gmail.com>
2024-01-02 22:58:59 +0100

    implemented list accounts controller

--- api/account.go
+++ api/account.go
@@ -59,3 +59,28 @@ func (server *Server) getAccount(ctx *gin.Context) {
 
 	ctx.JSON(http.StatusOK, account)
 }
+
+type listAccountsRequest struct {
+	PageID int32 `form:"page_id" binding:"required,min=1"`
+	PageSize int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req listAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	accounts, err := server.store.ListAccounts(ctx, db.ListAccountsParams{ 
+		Limit: int64(req.PageSize),
+		Offset: (int64(req.PageID - 1) * int64(req.PageSize)),
+	})
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, accounts)
+}
