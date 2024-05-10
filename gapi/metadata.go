15324841727c83540fe2312dbf1a6d1738331e84
Javier R <fjrpascual@gmail.com>
2024-05-10 22:18:09 +0200

    obtain user agent and client ip from metadata

---
+++ gapi/metadata.go
@@ -0,0 +1,46 @@
+package gapi
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/peer"
+)
+
+const (
+	grpcGatewayUserHeader = "grpcgateway-user-agent"
+	userAgentHeader = "user-agent"
+	xForwardedForHeader = "x-forwarded-for"
+)
+
+type Metadata struct {
+	UserAgent string
+	ClientIP string
+}
+
+func (server *Server) extractMetadata(ctx context.Context) *Metadata {
+	mtdt := &Metadata{}
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		// grpc gateway
+		if userAgents := md.Get(grpcGatewayUserHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+		// grpc
+		if userAgents := md.Get(userAgentHeader); len(userAgents) > 0 {
+			mtdt.UserAgent = userAgents[0]
+		}
+		// grpc gateway
+		if clientIPs := md.Get(xForwardedForHeader); len(clientIPs) > 0 {
+			mtdt.ClientIP = clientIPs[0]
+		}
+	}
+	
+	// grpc
+	if peer, ok := peer.FromContext(ctx); ok {
+		mtdt.ClientIP = peer.Addr.String()
+	}
+
+	return mtdt
+}
